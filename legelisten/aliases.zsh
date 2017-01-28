ef3e03e235fe49ddd84dcd5ae99f110ff0986527
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2017-01-28 18:16:29 +0100

    Add some more legelisten related aliases

--- legelisten/aliases.zsh
+++ legelisten/aliases.zsh
@@ -1,4 +1,11 @@
+alias h='heroku'
+alias hr='heroku run'
+
+alias -g prod='legelisten-prod'
+alias -g staging='legelisten-staging'
+
 alias staging-logs='heroku logs -t -alegelisten-staging'
 alias staging-console='heroku run rails c -alegelisten-staging'
-alias legelisten_start='docker-compose -f docker-compose.yml -f docker-compose-linux.yml up'
 
+alias legelisten_start='docker-compose -f docker-compose.yml -f docker-compose-linux.yml up'
+alias legelisten_cli='docker exec -t -i legelisten_web_1 bash'
