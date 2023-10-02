131d83ef8082e5c4f3200b46c12c4a5952bb445b
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2023-10-02 14:32:08 +0200

    Use docker compose, not docker-compose

--- legelisten/aliases.zsh
+++ legelisten/aliases.zsh
@@ -7,5 +7,5 @@ alias -g staging='legelisten-staging'
 alias staging-logs='heroku logs -t -alegelisten-staging'
 alias staging-console='heroku run rails c -alegelisten-staging'
 
-alias legelisten_start='docker-compose -f docker-compose.yml -f docker-compose-linux.yml up'
-alias legelisten_cli='docker exec -t -i legelisten_web_1 bash'
+alias legelisten_start='docker compose -f docker-compose.yml -f docker-compose-linux.yml up'
+alias legelisten_cli='docker exec -t -i legelisten-web-1 bash'
