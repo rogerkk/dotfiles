105ac2baaf7dc0385002087e13a1af6f802d1171
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2025-03-04 14:01:51 +0100

    Update nvim plugin setup

--- nvim/.config/nvim/lua/custom/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/custom/plugins/markdown-preview.lua
@@ -2,5 +2,5 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = ":call mkdp#util#install()",
 }
